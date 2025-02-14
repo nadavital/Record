94971af8e070d693e5378f5a882859e0558af1ff
Nadav Avital <nadav.avital@icloud.com>
2025-02-14 14:21:52 -0800

    Initial Commit

---
+++ record/recordApp.swift
@@ -0,0 +1,17 @@
+//
+//  recordApp.swift
+//  record
+//
+//  Created by Nadav Avital on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct recordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
